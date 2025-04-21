e216628b046578b5683e9ace54ea4f4c69e25c95
S0umith29 <soumith.odu@gmail.com>
2025-04-21 17:03:44 +0000

    terraform k3s

--- control-plane/k3cloudinit.tf
+++ control-plane/k3cloudinit.tf
@@ -13,7 +13,7 @@ packages:
 users:
   - name: soumith
     ssh-authorized-keys:
-      - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCqlVXhh5pO9CtcBDCVLQs9WQFjbA2GpgBpD106LicgZVWFwH0MYFodhhJEY2927ts6vP4jftBZP1KI8HjTOEFkbNkOiRTRo5Dag4VONjG6L+OrxqpvfbhSMTJkSu59b7fJZdX5VNrHU/QYXjNNUcPELMNUKSO/IKUUxYtEXZSG8gPomjLcUI+K6Ew8Q8okyzHfK3d+O5gUKYrhue6unqutCdTo+eUR7OSyLRCAyJiccaxC+X8OW2ZdvdWT77VAfJbOBFVZkqOXE7q5aGpvHwZupuOkjALSJw8UPXMFKV+EwjE6kx8hOO5Yu6xzYB4v7yHlk0bK7kMMjKOq5FSsAbl1kmV8c1ZkBQRNrngah0CdA8R8MsDEoDvZnq5XfDCwNQfwWhe5tL2SZOdgpDHRbr3fdH+l+lFaaNIso6RdbWkzUJ1tK2aNxDkSufZPYC5SzQqfw8HNSbvu+AKHa0noCil9Y5F0EFc/1usand0pbSDoNgS8PDB7TMvoV3ceEpQuzjc9k1frPYQmO6kXeYT+SPMxnk07+94/eQftsv7CtpnYX9n5CcwihKd1HtxjeAAxX299E4iCCdrj2zGlPh2U3AhR9QGlg6QqZ/HOo/11ocOtha5ITgEhNj+qdeuuNPNhLps7qm0FoineEcDt/BThfPekWJqWgrh2oMrnKcW8cepMJw== skuppave@odu.edu
+      - ssh-rsa YOUR_SSH_PUBLIC_KEY
     sudo: ALL=(ALL) NOPASSWD:ALL
     groups: sudo
     shell: /bin/bash
